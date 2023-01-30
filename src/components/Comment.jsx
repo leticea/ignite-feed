5ecb09c247ade92ad4d720ff72fd39e8e3641304
leticea <nascimento.leticeah@gmail.com>
2023-01-30 16:17:16 -0300

    criando props para o avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/leticea.png" alt="" />
+      <Avatar src="https://github.com/leticea.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>

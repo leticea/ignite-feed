c97d94bb45bff800d22e3159824f8dfdb90ba0b1
leticea <nascimento.leticeah@gmail.com>
2023-01-31 11:15:13 -0300

    criando conteúdo dos posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/leticea.png",
+      name: "Letícia Nascimento",
+      role: "Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋"},
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: "link", content: "jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2023-01-25 21:15:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educador @Rocketseat"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋"},
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      { type: "link", content: "jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2023-01-29 21:15:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,9 +45,7 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-
-          <Post />
-          <Post />
+          {posts}
 
         </main>
       </div>

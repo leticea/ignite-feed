8f5e1f5acf6a76155dfde1a28f7c610cc62e4f3e
leticea <nascimento.leticeah@gmail.com>
2023-01-31 11:57:31 -0300

    adicionando parágrafo em cada linha do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,14 +5,14 @@ import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post({ author, publishedAt }) {
+export function Post({ author, publishedAt, content }) {
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
   })
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
-    addSuffix: true 
+    addSuffix: true
   })
 
   return (
@@ -26,26 +26,21 @@ export function Post({ author, publishedAt }) {
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime="2023-01-10 08:13:30">
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
           {publishedDateRelativeToNow}
 
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+
+          } else if (line.type === "link") {
+            return <p><a href="#">{line.content}</a></p>;
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
